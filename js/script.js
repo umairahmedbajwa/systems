7d09bb07a4b02246d7cf8922bed6f04cf0e50743
umairahmedbajwa <umairahmed.bajwa@systemsltd.com>
2022-01-04 15:16:34 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+	$(".main-content").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		vertical: true,
+		asNavFor: ".feature-hold",
+	});
+	$(".feature-hold").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: ".main-content",
+		dots: true,
+		arrows: false,
+		infinite: true,
+		nextArrow: ".right-arrow",
+		prevArrow: ".left-arrow",
+		vertical: true,
+		focusOnSelect: true,
+	});
+});
+
+$(document).ready(function () {
+	$(".happy-slidmain").slick({
+		autoplay: true,
+		arrows: false,
+		dots: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 1000,
+	});
+});
+
+$(document).ready(function () {
+	$(".award-slider").slick({
+		autoplay: true,
+		arrows: false,
+		dots: false,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		speed: 1000,
+	});
+});
